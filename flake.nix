7f2ceed7acd163d7db2804b0d1786421e1e17b83
Glucon <ombindraindode@proton.me>
2025-08-20 22:53:20 +0800

    chore: naersk follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -5,7 +5,10 @@
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
-    naersk.url = "github:nix-community/naersk";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     git-hooks = {
       url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -15,58 +18,81 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { flake-parts, naersk, ... }@inputs:
+  outputs =
+    { flake-parts, naersk, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ inputs.git-hooks.flakeModule inputs.treefmt-nix.flakeModule ];
-      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      perSystem = { config, self', pkgs, ... }: {
-        pre-commit = {
-          check.enable = true;
-          settings.hooks = {
-            nixpkgs-fmt.enable = true;
-            shfmt.enable = true;
-            taplo.enable = true;
-            prettier = {
-              enable = true;
-              excludes = [ "flake.lock" ];
+      imports = [
+        inputs.git-hooks.flakeModule
+        inputs.treefmt-nix.flakeModule
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          pkgs,
+          ...
+        }:
+        {
+          pre-commit = {
+            check.enable = true;
+            settings.hooks = {
+              nixfmt-rfc-style.enable = true;
+              shfmt.enable = true;
+              taplo.enable = true;
+              prettier = {
+                enable = true;
+                excludes = [ "flake.lock" ];
+              };
+              rustfmt.enable = true;
             };
-            rustfmt.enable = true;
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+              shfmt.enable = true;
+              taplo.enable = true;
+              prettier.enable = true;
+              rustfmt.enable = true;
+            };
+            settings.global = {
+              excludes = [ ];
+            };
+          };
+
+          packages.yd =
+            let
+              naersk' = pkgs.callPackage naersk { };
+            in
+            naersk'.buildPackage { src = ./.; };
+
+          apps.yd = {
+            type = "app";
+            program = "${self'.packages.yd}/bin/yd";
+          };
+
+          packages.default = self'.packages.yd;
+          apps.default = self'.apps.yd;
+
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [
+              config.pre-commit.devShell
+              config.treefmt.build.devShell
+            ];
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              cargo
+              rustc
+            ];
+            buildInputs = with pkgs; [ openssl ];
           };
         };
-
-        treefmt = {
-          projectRootFile = "flake.nix";
-          programs = {
-            nixpkgs-fmt.enable = true;
-            shfmt.enable = true;
-            taplo.enable = true;
-            prettier.enable = true;
-            rustfmt.enable = true;
-          };
-          settings.global = {
-            excludes = [ ];
-          };
-        };
-
-        packages.yd =
-          let
-            naersk' = pkgs.callPackage naersk { };
-          in
-          naersk'.buildPackage { src = ./.; };
-
-        apps.yd = {
-          type = "app";
-          program = "${self'.packages.yd}/bin/yd";
-        };
-
-        packages.default = self'.packages.yd;
-        apps.default = self'.apps.yd;
-
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ config.pre-commit.devShell config.treefmt.build.devShell ];
-          nativeBuildInputs = with pkgs;[ pkg-config cargo rustc ];
-          buildInputs = with pkgs; [ openssl ];
-        };
-      };
     };
 }
